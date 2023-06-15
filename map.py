fd271d7522facd696f220053d1e0cea611177257
Sussy Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2023-06-15 14:41:40 +0800

    Update map.python has

--- map.py
+++ map.py
@@ -1,14 +1,17 @@
 from colours import *
 
 groundMap = f"""
-                     Secret Base - - - - {cyan}Northern Tower{reset} - - - {red}Reiltas Ruins{reset}
-                                                    |
-       {blue}Dabirc City{reset} - {yellow}Nellight Town{reset} - - - - - {green}Reiltas Town{reset}                     
-                  |                      |        \                                                                     {yellow}Marusha Town{reset}
-                   \                 Fallen Cave    \ - - - - {red}Mt. Foxclore{reset} - - - - - {blue}Vasagle City{reset}     (1001)
-                (                                                                                      (1015)              (1002)
-                    \                                                                                      /                  /
-                  {blue}Bronea City{reset} - - {yellow}Skaxis City{reset} - - - {green}Mt. Crimson{reset} - - - - {yellow}Feandra City{reset}
-                          \                            |                               
-      
+                        Secret Base - - - - {cyan}Northern Tower{reset} - - - {red}Reiltas Ruins{reset}
+                                                          |
+               {blue}Dabirc City{reset} - {yellow}Nellight Town{reset} - - - - - {green}Reiltas Town{reset}                     
+                       |                      |        \                                                                                      {yellow}Marusha Town{reset}
+                        \                 Fallen Cave    \ - - - - {red}Mt. Foxclore{reset} - - - - - {blue}Vasagle City{reset}                         (1001) 
+                         (                                                                                      (1015)                                 (1002)
+                          \                                                                                      /                                       /
+{purple}Underground Entrace{reset}   -   -   -  {blue}Bronea City{reset} - - {yellow}Skaxis City{reset} - - - {green}Mt. Crimson{reset} - - - - {yellow}Feandra City{reset}
+                                                         |                                                                                           (1003)
+                                                         |                                {red}Power Plant{reset}                                       |
+                                                          \                                           |                                                
+                                                {cyan}Ancient Tower{reset} - {green}Loodon Village{reset} - {red}Dawn Fortress{reset}      {blue}Silek City{reset}
 """
+      
