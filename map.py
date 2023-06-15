05a5836169f57b4ffba62c8d2da4f2ee415033db
Sussy Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2023-06-15 09:58:32 +0800

    Update map.py

--- map.py
+++ map.py
@@ -1,9 +1,14 @@
 from colours import *
 
 groundMap = f"""
-
-       {blue}Dabirc City{reset} - Nellight Town \
-                  |                      |        \
-                   \                 Fallen Cave    \ - 
-                    \
+                     Secret Base - - - - {cyan}Northern Tower{reset} - - - {red}Reiltas Ruins{reset}
+                                                    |
+       {blue}Dabirc City{reset} - {yellow}Nellight Town{reset} - - - - - {green}Reiltas Town{reset}                     
+                  |                      |        \                                                                     {yellow}Marusha Town{reset}
+                   \                 Fallen Cave    \ - - - - {red}Mt. Foxclore{reset} - - - - - {blue}Vasagle City{reset}     (1001)
+                                                                                                          (1015)              (1002)
+                    \                                                                                      /                  /
+                  {blue}Bronea City{reset} - - {yellow}Skaxis City{reset} - - - {green}Mt. Crimson{reset} - - - - {yellow}Feandra City{reset}
+                          \                            |                               
+      
 """
